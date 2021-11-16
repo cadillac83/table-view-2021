94e7a48b942d1572eed33ac9a021a7fc273d045d
kennylikai@163.com <kennylikai@163.com>
2021-11-16 16:49:22 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <TableView />
+</template>
+
+<script>
+import TableView from "./components/TableView.vue";
+
+export default {
+  name: "App",
+  components: {
+    TableView,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
